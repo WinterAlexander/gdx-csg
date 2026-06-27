ea7ebf4759c7c806a3f36e79beaa7f534f90660f
Alexander Winter <a.w1nter@hotmail.com>
2026-06-27 10:19:34 -0400

    Fixed wrench case, problem was in gdx-utils SegmentPlus

--- src/test/java/com/winteralexander/gdx/csg/test/CSGMeshWithBuilderTest.java
+++ src/test/java/com/winteralexander/gdx/csg/test/CSGMeshWithBuilderTest.java
@@ -6,7 +6,19 @@ import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.CylinderShapeBuilder;
+import com.badlogic.gdx.utils.IntArray;
+import com.winteralexander.gdx.csg.CSGMesh;
+import com.winteralexander.gdx.csg.CSGUtil;
+import com.winteralexander.gdx.csg.test.debugviewer.CSGMeshViewer;
+import com.winteralexander.gdx.csg.test.debugviewer.TriangleViewer;
+import com.winteralexander.gdx.utils.collection.CollectionUtil;
+import com.winteralexander.gdx.utils.math.shape3d.Intersector3D;
+import com.winteralexander.gdx.utils.math.shape3d.SegmentPlus;
+import com.winteralexander.gdx.utils.math.shape3d.Triangle;
 import org.junit.Test;
+
+import static com.winteralexander.gdx.utils.math.shape3d.Intersector3D.intersectTriangleTriangle;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Tests the CSGMesh operations with a libGDX model builder, which is convenient since it requires
@@ -27,7 +39,17 @@ public class CSGMeshWithBuilderTest {
 				| VertexAttributes.Usage.Tangent, new Material());
 
 		CylinderShapeBuilder.build(partBuilder, 0.15f, 0.02f, 0.15f, 10);
+		CSGMesh csgMesh = CSGMesh.fromBuilder(partBuilder);
+		csgMesh.getVertices().forEach(v -> v.getPosition().add(0f, 0f, -0.5f));
 
+		int idxStart = partBuilder.getNumVertices();
+		CylinderShapeBuilder.build(partBuilder, 0.05f, 0.02f, 0.05f, 10);
 
+		CSGMesh substrahend = CSGMesh.fromBuilder(partBuilder,
+				CollectionUtil.arrayFromRange(idxStart, partBuilder.getNumVertices()));
+		substrahend.getVertices().forEach(v -> v.getPosition().add(-0.05f, 0f, -0.5f));
+
+		CSGUtil.subtraction(csgMesh, substrahend);
+		// test is to ensure this doesn't crash
 	}
 }
