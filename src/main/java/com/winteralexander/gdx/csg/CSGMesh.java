5d57b78d341a1228765a009db5f81bdc244c78d1
Alexander Winter <a.w1nter@hotmail.com>
2026-07-08 22:07:02 -0400

    Recursive split was stupid

--- src/main/java/com/winteralexander/gdx/csg/CSGMesh.java
+++ src/main/java/com/winteralexander/gdx/csg/CSGMesh.java
@@ -102,7 +102,7 @@ public class CSGMesh implements Serializable {
 		this.faces.addAll(other.faces);
 	}
 
-	private boolean splitIfNeeded(int index, MeshFace face, MeshFace otherFace) {
+	private void splitIfNeeded(int index, MeshFace face, MeshFace otherFace) {
 		TriangleIntersectionResult result = intersectTriangleTriangle(face.getTriangle(),
 				otherFace.getTriangle(),
 				config.tolerance,
@@ -110,7 +110,7 @@ public class CSGMesh implements Serializable {
 		if(result == NONCOPLANAR_FACE_FACE) {
 			cutEdges.add(intersectSegment.cpy());
 			plane.set(otherFace.getPosition1(), otherFace.getNormal());
-			return splitFace(index, plane);
+			splitFace(index, plane);
 		} else if(result == EDGE_FACE) {
 			boolean isEdgeFromFace = false;
 			for(int j = 0; j < 3; j++) {
@@ -131,23 +131,17 @@ public class CSGMesh implements Serializable {
 			if(!isEdgeFromFace) {
 				cutEdges.add(intersectSegment.cpy());
 				plane.set(otherFace.getPosition1(), otherFace.getNormal());
-				return splitFace(index, plane);
+				splitFace(index, plane);
 			}
 		}
-		return false;
 	}
 
 	public void splitTriangles(CSGMesh other) {
 		tmpNewVertices.clear();
 		boundaryFaces.clear();
-		boolean splitOne;
-		do {
-			splitOne = false;
-			for(int i = 0; i < faces.size; i++)
-				for(MeshFace otherFace : other.faces)
-					// given splitFace may modify the faces array, must not put this at the outer level
-					splitOne |= splitIfNeeded(i, faces.get(i), otherFace);
-		} while(splitOne);
+		for(int i = 0; i < faces.size; i++)
+			for(MeshFace otherFace : other.faces)
+				splitIfNeeded(i, faces.get(i), otherFace);
 		tmpNewVertices.clear();
 
 		if(config.enableBoundaryFaces)
@@ -192,13 +186,13 @@ public class CSGMesh implements Serializable {
 		}
 	}
 
-	private boolean splitFace(int faceIndex, Plane plane) {
+	private void splitFace(int faceIndex, Plane plane) {
 		MeshFace face = faces.get(faceIndex);
 		face.getTriangle().toArray(tmpArray);
 		Intersector.splitTriangle(tmpArray, plane, splitTriangle);
 
 		if(splitTriangle.total == 1)
-			return false;
+			return;
 
 		if(splitTriangle.numBack == 0 && splitTriangle.numFront == 0)
 			throw new IllegalStateException("Split face has no split result");
@@ -208,14 +202,11 @@ public class CSGMesh implements Serializable {
 
 		for(int i = 0; i < splitTriangle.numFront; i++)
 			processSplitTriangle(face, splitTriangle.front, i * 9);
-		if(toAdd.size == 0)
-			return false;
 
 		faces.set(faceIndex, toAdd.get(0));
 		faces.addAll(toAdd, 1, toAdd.size - 1);
 
 		toAdd.clear();
-		return true;
 	}
 
 	private boolean checkForMergeWithNeighbors(MeshFace face) {
--- src/test/java/com/winteralexander/gdx/csg/test/CSGMeshWithBuilderTest.java
+++ src/test/java/com/winteralexander/gdx/csg/test/CSGMeshWithBuilderTest.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.CylinderShapeBuilder;
 import com.winteralexander.gdx.csg.CSGConfiguration;
 import com.winteralexander.gdx.csg.CSGMesh;
 import com.winteralexander.gdx.csg.CSGUtil;
-import com.winteralexander.gdx.csg.test.debugviewer.ModelViewer;
 import com.winteralexander.gdx.utils.collection.CollectionUtil;
 import org.junit.Test;
 
@@ -47,7 +46,7 @@ public class CSGMeshWithBuilderTest {
 		}});
 		result.toBuilder(partBuilder, CollectionUtil.arrayFromRange(0, partBuilder.getNumVertices()));
 		// test is to ensure this doesn't crash
-		ModelViewer.start(builder);
+		// ModelViewer.start(builder);
 	}
 
 	@Test
